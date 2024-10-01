4e03ddafdbc1accbc11a7ee80c6c0ce66b8a0edf
Son Bui <ngocson.bui0312@gmail.com>
2024-10-01 21:51:35 +0300

    templating the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { useContext, useEffect } from 'react';
+import { UserContext } from './context/UserContext';
+import UserTable from './components/UserTable';
+import Header from './components/Header';
 
-export default App;
+export const App = () => {
+  const { fetchData } = useContext(UserContext);
+  useEffect(() => {
+    if (fetchData !== undefined) fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <UserTable />
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.tsx';
+import { UserContextProvider } from './context/UserContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </StrictMode>
+);
