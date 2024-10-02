fd9f20481a87823fc3d914c57f75120796ac9ebb
Son Bui <ngocson.bui0312@gmail.com>
2024-10-03 01:39:04 +0300

    moved filter basic logic into table logic

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -3,13 +3,15 @@ import { UserContext } from '../context/UserContext';
 
 const UserTable = () => {
   const { data } = useContext(UserContext);
+  const [filterTerm, setFilterTerm] = useState('');
+  const [ascend, setAscend] = useState(false);
 
   useEffect(() => {}, []);
 
   const renderTable = () => {
     if (typeof data === 'string') return <>Failed to fetch data</>;
     return data?.map((i) => (
-      <div key={i.id} style={{ height: '10vh' }}>
+      <div key={i.id} style={{ height: '6vh' }}>
         {i.name}
       </div>
     ));
@@ -17,6 +19,17 @@ const UserTable = () => {
 
   return (
     <div style={{ display: 'block', height: 'fit-content' }}>
+      <div>
+        <input
+          value={filterTerm}
+          type="string"
+          onChange={(e) => setFilterTerm(e.target.value)}
+        />
+        <>{filterTerm}</>
+        <div onClick={() => setAscend(!ascend)}>
+          {ascend ? 'ascending' : 'descending'}
+        </div>
+      </div>
       {renderTable()}
     </div>
   );
