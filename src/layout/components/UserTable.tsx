c071b725306fd04c5463a8dfa12d1a7044117ab4
Son Bui <ngocson.bui0312@gmail.com>
2024-10-03 02:42:34 +0300

    adding sorting function

--- src/layout/components/UserTable.tsx
+++ src/layout/components/UserTable.tsx
@@ -5,13 +5,15 @@ import UserCard from './UserCard';
 const UserTable = () => {
   const { data } = useContext(UserContext);
   const [filterTerm, setFilterTerm] = useState('');
-  const [ascend, setAscend] = useState(false);
+  const [ascend, setAscend] = useState(1);
 
   useEffect(() => {}, []);
 
   const renderTable = () => {
     if (typeof data === 'string') return <>Failed to fetch data</>;
-    return data?.map((i) => <UserCard key={i.id} user={i} />);
+    return data
+      ?.sort((i, k) => (i.name > k.name ? 1 : -1) * ascend)
+      .map((i) => <UserCard key={i.id} user={i} />);
   };
 
   return (
@@ -23,8 +25,8 @@ const UserTable = () => {
           onChange={(e) => setFilterTerm(e.target.value)}
         />
         <>{filterTerm}</>
-        <button onClick={() => setAscend(!ascend)}>
-          {ascend ? 'ascending' : 'descending'}
+        <button onClick={() => setAscend(ascend * -1)}>
+          {ascend === 1 ? 'ascending' : 'descending'}
         </button>
       </div>
       {renderTable()}
